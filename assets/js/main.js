c5ef5cfe8f71e773451b2277449ed11530d4c31e
peterimoh <peterimoh2@gmail.com>
2020-12-10 21:52:47 +0100

    added hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
 function openbtn() {
-    document.querySelector('.closebtn').style.display = "block";
-    document.querySelector('.closebtn').style.transition = "10s";
-
-    document.querySelector('.navigation').style.display = 'none';
+  document.querySelector(".closebtn").style.display = "block";
+  document.querySelector(".closebtn").style.transition = "width 4s";
+  document.querySelector(".navigation").style.display = "none";
 }
 
 function closebtn() {
-    document.querySelector(".closebtn").style.display = "none";
-    document.querySelector(".navigation").style.display = "flex";
+  document.querySelector(".closebtn").style.display = "none";
+  document.querySelector(".navigation").style.display = "flex";
 }
